03f6dfcb99c326246f04cb3c3a8db55089b5588a
이영빈 <yblee@itaz.kr>
2022-04-11 15:08:20 +0900

    Revert "로그인 처리부분 작성중..."
    This reverts commit 893f98cdb3e568d3c3f6841841aec018c8166c08.

--- src/main/java/com/soft/controller/UserController.java
+++ src/main/java/com/soft/controller/UserController.java
@@ -1,48 +1,10 @@
 package com.soft.controller;
 
-
-
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.soft.service.UserService;
-import com.soft.vo.UserVO;
-
 @Controller
-@RequestMapping("Index")
 public class UserController {
 
-	private static final Logger logger =
-			LoggerFactory.getLogger(UserController.class);
-	
-	@Inject
-	private UserService userService;
-	
-	//GET METHOD
-	@RequestMapping("/join")
-	public void UserJoinGET(UserVO vo) throws Exception{
-		
-		logger.info("회원가입 완료.");
-		
-	}
-	
-	// 정보처리 액션
-	@RequestMapping("/joinAction")
-	public void UserJoinPOST(UserVO vo) throws Exception{
-		
-		logger.info("로그인 처리한 값을 받아서, 회원가입 처리를 완료하였습니다.");
-		
-		System.out.println(vo);
-		
-		//로그인 처리함.
-		//service.userJoin(vo);
-		
-		
-	}
-	
 	
 }
